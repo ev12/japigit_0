2d858123f62189c45815fe1c1af2b262527199c1
ev12 <notericv@gmail.com>
2020-03-09 17:04:25 -0700

    comment string

---
+++ japi.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import urllib.request,json
+
+def getStockData(stockSymbol):
+    url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol='
+    apikey = '&apikey=GUV66RNA99LL56XW'
+    fullUrl = url+stockSymbol+apikey
+    connection = urllib.request.urlopen(fullUrl)
+    responseString = connection.read().decode()
+    dictionary = json.loads(responseString)
+
+    print (responseString)
+    return dictionary
+
+def main():
+    file = open('japi.out','w')
+    while True:
+        stockSymbol = input("Enter Stock Symbol or quit to exit: ").upper()
+        if stockSymbol != 'QUIT':
+            stockInfo = getStockData(stockSymbol)
+            stockPrice = stockInfo['Global Quote']['05. price']
+            stockPrice = float(stockPrice)
+            output = 'The current price of '+stockSymbol+' is: '+'${:,.2f}'.format(stockPrice)+'\n'
+
+            file.write(output)
+
+        else:
+            print('Goodbye!')
+            break
+main()
